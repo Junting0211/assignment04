5759ff03be6129ffee148d4b6c2dddca681f76f1
Jialin Huo <jhuo3@u.rochester.edu>
2019-10-04 16:14:45 -0400

    upload index file

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+$currentPage = "Assignment 04: Odd Student Out";
+include "inc/html-top.inc";
+?>
+
+	<body>
+		<header>
+			<a href="index.php"></a>
+			<h1>Team Seattle</h1>
+		</header>
+
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="islomzhan.php">Islomzhan Akhmedov</a></li>
+				<li><a href="dewey.php">Dewey Bazirake</a></li>
+				<li><a href="xueying.php">Xueying Chen</a></li>
+			</ul>
+		</nav>
+
+		<div>
+			<img src="" alt="">
+			<img src="" alt="">
+			<img src="" alt="">
+		</div><!--.slider-->
+
+		<main>
+			<p>These three students, Islomzhan, Dewey and Xueying are University of Rochester seniors who are currently enrolling in CSC174 and having strong interests in web page designs. Two of them, Islomzhan and Dewey, have chosen Computer Science as their major. And Xueying, is very special among them since she is a Business Entrepreneurship major. 
+			</p>
+		</main>
+
+		<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+	</body>
+</html>
