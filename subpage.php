a21e53e21ca30b24dd2cf59dca8d048c4a61b1a8
Ana Acuna <anasofacuna@gmail.com>
2019-10-08 20:49:29 -0400

    f pattern on subpage for cs majors

--- subpage.php
+++ subpage.php
@@ -18,7 +18,7 @@ include "inc/html-top.inc";
 	<h2>Meet the Computer Science Students</h2>
 	<section>
 		<h3>Islomzhan Akhmedov</h3>
-		<img src="images/islomzhan.jpg" alt="islomzhan">
+		<img src="images/islomzhan.jpg" alt="islomzhan" class="islomzhan">
 		<p>I am a student at University of Rochester and my major is Computer Science. I will graduate the school in this December. Previously, I studied at North American University and there I worked as a part-time front web developer. I designed there the college's sports and IT websites. Recently, I started to work on a website for a toy store and I'm still working on the design and integrating ecommerce functionalities to it. Also, I am developing a mobile application for selling local farm goods. Here is a list of the projects I have worked on. Toyzland.com - this is a website that sells toys online. I made this ecommerce website for my friend's store. LoyalFarm - it is a mobile app that lets people to buy their groceries online. I am now working on this project and soon it will be possible to use, hopefully, before the end of this year. SpaceShooter - two years ago, I made this 2D space game that even the spaceships were designed by me.</p>
 	</section>
 	<section>
