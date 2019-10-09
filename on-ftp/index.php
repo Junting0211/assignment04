1c8786443515d85e7c66fbec5b1b72d3eceac33f
Ana Acuna <33498256+anasofacuna@users.noreply.github.com>
2019-10-09 16:42:54 -0400

    Add files via upload

---
+++ on-ftp/index.php
@@ -0,0 +1,70 @@
+<?php 
+$currentPage = "Assignment 04: Odd Student Out";
+include "inc/html-top.inc";
+?>
+
+<body class="z-pattern">
+<header class="persistent">
+	<div class="container">
+	<section>
+		<!-- Primary Optical Area -->
+		<h1>"Odd" Student "Out"</h1>
+	</section>
+	<section class="follow">
+		<!-- Strong Follow Area -->				
+		<h1>Team Seattle</h1>
+	</section>
+	</div>
+</header>
+
+<main>
+<div class="big-message">
+	
+	<section>
+	<div class="slideshow">
+
+  <img class= "Slides" src="images/bell-harbor.jpg" alt="BellHarbor">
+  <img class= "Slides" src="images/locals-seattle.jpg" alt="locals_Seattle">
+  <img class= "Slides" src="images/Aerial-looking.jpg" alt="Aerial-Looking">
+
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+	</section><!--.slider-->
+
+
+	<section class="intro">
+		<p>These three students, Islomzhan, Dewey and Xueying are University of Rochester seniors who are currently enrolling in CSC174 and having strong interests in web page designs. Two of them, Islomzhan and Dewey, have chosen Computer Science as their major. And Xueying, is very special among them since she is a Business Entrepreneurship major. </p>
+	</section>
+	</div>
+</main>
+
+<footer class="persistent">
+	<div class="container">
+	<section>
+		<!-- Weak Visual Area -->
+		CSC 174: Advanced Front-end Web Design and Development
+	</section>
+	<section>
+		<!-- Terminal Area -->
+		<a href="subpage.php">Click Here to Learn More</a>
+	</section>
+	</div>
+</footer>
+
+<script src="js/menu-highlighter.js"></script>
+ 
+      <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+      <script>
+        $('.slider').sss();
+      </script>
+
+      <script>
+        $( "#accordion" ).accordion({
+          heightStyle: "content"
+        });
+      </script>
+</body>
+</html>
---
+++ on-ftp/subpage.php
@@ -0,0 +1,84 @@
+<?php 
+$currentPage = "Learn More | Assignment 04: Odd Student Out";
+include "inc/html-top.inc";
+?>
+
+
+<body class="f-pattern">
+<header>
+	<section>
+		<h1>Learn More About Us</h1>
+	</section>
+	<nav>
+		<a href="index.php">Home</a>
+	</nav>
+</header>
+	
+<article>
+	<div class="cs-majors">
+	<h2>Meet the Computer Science Students</h2>
+	<section>
+		<h3>Islomzhan Akhmedov</h3>
+		<img src="images/islomzhan.jpg" alt="islomzhan" class="islomzhan">
+		<p>I am a student at University of Rochester and my major is Computer Science. I will graduate the school in this December. Previously, I studied at North American University and there I worked as a part-time front web developer. I designed there the college's sports and IT websites. Recently, I started to work on a website for a toy store and I'm still working on the design and integrating ecommerce functionalities to it. Also, I am developing a mobile application for selling local farm goods. Here is a list of the projects I have worked on. Toyzland.com - this is a website that sells toys online. I made this ecommerce website for my friend's store. LoyalFarm - it is a mobile app that lets people to buy their groceries online. I am now working on this project and soon it will be possible to use, hopefully, before the end of this year. SpaceShooter - two years ago, I made this 2D space game that even the spaceships were designed by me.</p>
+	</section>
+	<section>
+		<h3>Dewey Bazirake</h3>
+		<img src="images/dewey.jpg" alt="dewey">
+		<p>I am currently a senior who is majoring in Computer Science. I am also an international student from Uganda, East Africa currently studying in the United States. My belief in education to make a positive change has always been a driving factor in my academics, work and travel. My hope is that to learn the necessary skills in computer science, finance and management to lead organizations and companies that will make a fundamental change in our world. My love for numbers and making a change in the world has resulted in me taking an interest in Business, Computer Science & Robotics. During my Junior year, I went to study abroad in Shanghai, China which was a life changing experience. Aside from the great food that I enjoyed, I got a chance to put my python programming skills into practice. I worked with a startup called, MIRO AI - a sports AI company. </p>
+	</section>	
+	</div>
+	<section class="odd">
+		<h2>Take a Look at the Special Business Major</h2>
+		<h3>Xueying (Shelly) Chen</h3>
+		<img src="images/shelley.jpg" alt="shelly">
+		<p>I am senior at University of Rochester and I'm majoring in Business Entrepreneurship. I am a researcher, activist and writer of social entrepreneurship and social innovation. I worked on a research project in Beijing and Shenzhen investigating how Chinese social entrepreneurs help migrant workers integrate into urban society in China. In the past year, I led MBA teams and undergraduate teams to compete in various business competitions including Tibetan Innovation challenge, Hult Prize and New York Business Plan competition. This past summer I interned for a fintech company "MoneyMatch" to come up with a SEO strategy to improve their website's google organic listing and came up with content for their social media channels. I also worked for a venture capital firm in Kuala Lumpur where I streamlined deal-sourcing and worked with entrepreneurs help them achieve their goals.</p>
+	</section>
+</article>
+
+<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+
+<script src="js/menu-highlighter.js"></script>
+      <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+      <script>
+        $('.slider').sss();
+      </script>
+
+      <script>
+        $( "#accordion" ).accordion({
+          heightStyle: "content"
+        });
+      </script>
+
+</body>
+</html>
